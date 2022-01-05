0ab4836b22975e682a7229bdb40ae7734c1c8694
Adrielle Krauchuki <adrielle.krauchuki@stone.com.br>
2022-01-04 21:32:22 -0300

    Create test class to Domain/Category

---
+++ CleanArquitecture.Domain.Test/Entities/CategoryTest.cs
@@ -0,0 +1,23 @@
+﻿using CleanArchitecture.Domain.Entities;
+using FluentAssertions;
+using Xunit;
+
+namespace CleanArquitecture.Domain.Test.Entities
+{
+    public class CategoryTest
+    {
+        [Fact(DisplayName = "Constructor without ID: Create category with valid state")]
+        public void Create_WithValidParameters_ResultObjectWithValidState()
+        {
+            var action = () => new Category("Category name");
+            action.Should().NotThrow<DomainExceptionValidation>();
+        }
+
+        [Fact(DisplayName = "Constructor with ID: Create category with valid state")]
+        public void Create_ConstructorWithIdWithValidParameters_ResultObjectWithValidState()
+        {
+            var action = () => new Category(10, "Category name");
+            action.Should().NotThrow<DomainExceptionValidation>();
+        }
+    }
+}

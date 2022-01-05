36868ec26b2239ef7a8545cc97a6f05702013a12
Adrielle Krauchuki <adrielle.krauchuki@stone.com.br>
2022-01-05 09:13:33 -0300

    Add Domain/Product test class

---
+++ CleanArquitecture.Domain.Test/Entities/ProductTest.cs
@@ -0,0 +1,20 @@
+﻿using CleanArchitecture.Domain.Entities;
+using FluentAssertions;
+using Xunit;
+
+namespace CleanArquitecture.Domain.Test.Entities
+{
+    public class ProductTest
+    {
+        [Fact(DisplayName = "Constructor with ID: ID negative must Throw DomainExceptionValidation")]
+        public void ConstructorWithId_WithNegativeIdValue_ThrowDomainExceptionValidation()
+        {
+            var action = () => new Product(-1, "Name", "Description", new decimal(100), 10, "Image");
+
+            action
+                .Should()
+                .Throw<DomainExceptionValidation>()
+                .WithMessage("Invalid ID value");
+        }
+    }
+}

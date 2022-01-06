f7793b680df3e9daf0db22edc2a94619b68e8c81
Adrielle Krauchuki <adrielle.krauchuki@stone.com.br>
2022-01-05 22:01:14 -0300

    Add Infra.Data/EntitiesConfiguration/ProductConfiguration

---
+++ CleanArchitecture.Infra.Data/EntitiesConfiguration/CategoryConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using CleanArchitecture.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchitecture.Infra.Data.EntitiesConfiguration
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(h => h.Id);
+            builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
+        }
+    }
+}
---
+++ CleanArchitecture.Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using CleanArchitecture.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchitecture.Infra.Data.EntitiesConfiguration
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(h => h.Id);
+
+            builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
+            builder.Property(p => p.Description).HasMaxLength(200).IsRequired();
+            builder.Property(p => p.Price).HasPrecision(10, 2);
+
+            builder.HasOne(h => h.Category).WithMany(w => w.Products).HasForeignKey(h => h.CategoryId);
+        }
+    }
+}

86c691c2644329a39d3188fd36112676b822499d
Adrielle Krauchuki <adrielle.krauchuki@stone.com.br>
2022-01-11 23:01:02 -0300

    Implement ProductRepository methods

--- CleanArchitecture.Domain/Interfaces/IProductRepository.cs
+++ CleanArchitecture.Domain/Interfaces/IProductRepository.cs
@@ -5,7 +5,7 @@ namespace CleanArchitecture.Domain.Interfaces
     public interface IProductRepository
     {
         Task<IEnumerable<Product>> GetAllAsync();
-        Task<Product> GetByIdAsync(int? id);
+        Task<Product?> GetByIdAsync(int? id);
         Task<Product> CreateAsync(Product product);
         Task<Product> UpdateAsync(Product product);
         Task<Product> RemoveAsync(Product product);
---
+++ CleanArchitecture.Infra.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,48 @@
+﻿using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Infra.Data.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Product> CreateAsync(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return product;
+        }
+
+        public async Task<IEnumerable<Product>> GetAllAsync() =>
+            await _context.Products.ToListAsync();
+
+
+        public async Task<Product?> GetByIdAsync(int? id) =>
+            await _context.Products.FindAsync(id);
+        
+
+        public async Task<Product> RemoveAsync(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return product;
+        }
+
+        public async Task<Product> UpdateAsync(Product product)
+        {
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+
+            return product;
+        }
+    }
+}

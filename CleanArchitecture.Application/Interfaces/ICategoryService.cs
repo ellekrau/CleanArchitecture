c010018b46332e7b4b86c50d990f73b059fe46e5
Adrielle Krauchuki <adrielle.krauchuki@stone.com.br>
2022-01-23 22:59:39 -0300

    Add CategoryService

---
+++ CleanArchitecture.Application/Interfaces/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using CleanArchitecture.Application.DTOs;
+
+namespace CleanArchitecture.Application.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<CategoryDTO>> GetCategories();
+        Task<CategoryDTO> GetById(int? id);
+        Task Create(CategoryDTO categoryDTO);
+        Task Update(CategoryDTO categoryDTO);
+        Task Delete(int? id);
+    }
+}
---
+++ CleanArchitecture.Application/Services/CategoryService.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using CleanArchitecture.Application.DTOs;
+using CleanArchitecture.Application.Interfaces;
+using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Domain.Interfaces;
+
+namespace CleanArchitecture.Application.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public CategoryService(IMapper mapper, ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        public async Task Create(CategoryDTO categoryDTO)
+        {
+            var category = _mapper.Map<Category>(categoryDTO);
+            await _categoryRepository.CreateAsync(category);
+        }
+
+        public async Task Delete(int? id)
+        {
+            var category = _categoryRepository.GetByIdAsync(id).Result;
+
+            if (category is not null)
+                await _categoryRepository.RemoveAsync(category);
+        }
+
+        public async Task<CategoryDTO> GetById(int? id)
+        {
+            var category = await _categoryRepository.GetByIdAsync(id);
+            return _mapper.Map<CategoryDTO>(category);
+        }
+
+        public async Task<IEnumerable<CategoryDTO>> GetCategories()
+        {
+            var categories = await _categoryRepository.GetAllAsync();
+            return _mapper.Map<IEnumerable<CategoryDTO>>(categories);
+        }
+
+        public async Task Update(CategoryDTO categoryDTO)
+        {
+            var category = _mapper.Map<Category>(categoryDTO);
+            await _categoryRepository.UpdateAsync(category);
+        }
+    }
+}

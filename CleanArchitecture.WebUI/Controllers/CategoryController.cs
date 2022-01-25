d6032f82d977b946c1256a94a74caf9f3c1d9aac
Adrielle Krauchuki <adrielle.krauchuki@stone.com.br>
2022-01-24 22:00:08 -0300

    Add CategoryController and GetAll endpoint

---
+++ CleanArchitecture.WebUI/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using CleanArchitecture.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.WebUI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _categoryService.GetAll();
+            return View(categories);
+        }
+    }
+}

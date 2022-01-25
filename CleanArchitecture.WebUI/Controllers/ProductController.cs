7cb8db923974cd374ecfc5777953a88173bd7f60
Adrielle Krauchuki <adrielle.krauchuki@stone.com.br>
2022-01-24 22:03:59 -0300

    Add ProductController and GetAll endpoint

---
+++ CleanArchitecture.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using CleanArchitecture.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productService.GetAll();
+            return View(products);
+        }
+    }
+}

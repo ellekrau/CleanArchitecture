f9f8ad2b6caf97be922141d64782443b64915748
Adrielle Krauchuki <adrielle.krauchuki@stone.com.br>
2022-01-31 22:12:36 -0300

    Add GetProductByIdQueryHandler

---
+++ CleanArchitecture.Application/Products/Handlers/GetProductByIdQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using CleanArchitecture.Application.Products.Queries;
+using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Domain.Interfaces;
+using MediatR;
+
+namespace CleanArchitecture.Application.Products.Handlers
+{
+    public class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdQuery, Product>
+    {
+        private readonly IProductRepository _productRepository;
+
+        public GetProductByIdQueryHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<Product> Handle(GetProductByIdQuery request, CancellationToken cancellationToken) =>
+            await _productRepository.GetByIdAsync(request.Id);
+    }
+}

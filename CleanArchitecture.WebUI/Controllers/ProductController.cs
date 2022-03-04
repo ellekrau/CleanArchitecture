207de6d40dd1fcd52d079ff102eeae6985a04779
Adrielle Krauchuki <adrielle.krauchuki@stone.com.br>
2022-03-04 15:29:20 -0300

    Add product images to detalis

--- CleanArchitecture.WebUI/Controllers/ProductController.cs
+++ CleanArchitecture.WebUI/Controllers/ProductController.cs
@@ -9,11 +9,14 @@ namespace CleanArchitecture.WebUI.Controllers
     {
         private readonly IProductService _productService;
         private readonly ICategoryService _categoryService;
+        private readonly IWebHostEnvironment _enviroment;
 
-        public ProductController(IProductService productService, ICategoryService categoryService)
+        public ProductController(IProductService productService, ICategoryService categoryService, 
+                                 IWebHostEnvironment enviroment)
         {
             _productService = productService;
             _categoryService = categoryService;
+            _enviroment = enviroment;
         }
 
         [HttpGet]
@@ -75,6 +78,22 @@ namespace CleanArchitecture.WebUI.Controllers
             await _productService.Delete(id);
 
            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id is null)
+                return NotFound();
+
+            var productDto = await _productService.GetById(id);
+
+            if (productDto is null)
+                return NotFound();
+
+            var images = Path.Combine(_enviroment.WebRootPath, "images\\" + productDto.Image);
+            ViewBag.ImageExist = System.IO.File.Exists(images);
+
+            return View(productDto);
         }
     }
 }

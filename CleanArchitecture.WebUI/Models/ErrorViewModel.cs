db406f6638af36b28e096779547512da8eac8cd1
Adrielle Krauchuki <adrielle.krauchuki@stone.com.br>
2022-03-04 15:44:36 -0300

    Remove unused Error view

--- CleanArchitecture.WebUI/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace CleanArchitecture.WebUI.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
